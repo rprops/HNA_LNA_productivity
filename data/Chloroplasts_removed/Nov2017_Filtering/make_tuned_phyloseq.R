47d8b70147fa514ce46ed4d5f634dbcc3d937c61
Marian L. Schmidt <marschmi@umich.edu>
2017-11-26 14:11:59 -0500

    Created phyloseq for phylogenetic analysis

--- data/Chloroplasts_removed/Nov2017_Filtering/make_tuned_phyloseq.R
+++ data/Chloroplasts_removed/Nov2017_Filtering/make_tuned_phyloseq.R
@@ -80,6 +80,58 @@ save(list=c("HNA_physeq_1in3","LNA_physeq_1in3"), file=paste0("HNA-LNA-physeq_1i
 
 
 
+################################################  
+################################################  ONE BIG PHYLOSEQ WITH HNA AND LNA SAMPLES = Phylogenetic Analysis
+### Make a phyloseq object with HNA and LNA "Samples" keeping the original taxonomy 
+# This object will be for phylogenetic analysis!
+rel_physeq_1in3 # Will keep tax_table and phy_tree but REPLACE otu_table
+
+############### LNA
+###  MAKE NEW OTU TABLE FOR LNA SAMPLES
+otu_LNA_1in3 <- otu_table(LNA_physeq_1in3) %>%
+  data.frame() %>%
+  # Make a new column from the rownames
+  tibble::rownames_to_column("names") %>%
+  # Make a new names column with _LNA at the end of each sample name
+  mutate(names = paste(names, "_LNA", sep="")) 
+
+############### HNA
+###  MAKE NEW OTU TABLE FOR HNA SAMPLES
+otu_HNA_1in3 <- otu_table(HNA_physeq_1in3) %>%
+  data.frame() %>%
+  # Make a new column from the rownames
+  tibble::rownames_to_column("names") %>%
+  # Make a new names column with _LNA at the end of each sample name
+  mutate(names = paste(names, "_HNA", sep="")) 
+
+
+# Full join with original OTU table 
+otu_all_1in3 <- otu_table(rel_physeq_1in3) %>%
+  data.frame() %>%
+  # Make a new column from the rownames
+  tibble::rownames_to_column("names")
+
+## Combine the regular, HNA, and LNA otus all into the same otu table
+otu_table_ALL_1in3 <- full_join(otu_all_1in3, otu_LNA_1in3) %>%
+  full_join(otu_HNA_1in3) %>%
+  # Replace all of the NAs with zeros
+  mutate_all(funs(ifelse(is.na(.), 0, .))) %>%
+  # Make the names to be rownames
+  tibble::column_to_rownames("names")
+
+# compatible with phyloseq
+#### MUST BE A NUMBER AND NOT AN NA!
+# Combine all the data together into one object
+ALL_rel_physeq_1in3 <- merge_phyloseq(otu_table(otu_table_ALL_1in3, taxa_are_rows = FALSE), 
+                                       tax_table(as.matrix(tax_1seq_in3samps)),
+                                       phy_tree(tree_1seq_in3samps))
+### See bottom of file for saving this phyloseq object!
+
+
+
+
+
+
 ########################################################################################  
 ############################### 5seq_in_10percent ######################################
 ########################################################################################  
@@ -152,6 +204,64 @@ save(list=c("HNA_physeq_5in10","LNA_physeq_5in10"), file=paste0("HNA-LNA-physeq_
   # This above line of code will create a single .RData object and with the 2 following phyloseq objects: 
         # 1. HNA_physeq_5in10
         # 2. LNA_physeq_5in10
+
+
+################################################  
+################################################  ONE BIG PHYLOSEQ WITH HNA AND LNA SAMPLES = Phylogenetic Analysis
+### Make a phyloseq object with HNA and LNA "Samples" keeping the original taxonomy 
+# This object will be for phylogenetic analysis!
+rel_physeq_5in10 # Will keep tax_table and phy_tree but REPLACE otu_table
+
+############### LNA
+###  MAKE NEW OTU TABLE FOR LNA SAMPLES
+otu_LNA_5in10 <- otu_table(LNA_physeq_5in10) %>%
+  data.frame() %>%
+  # Make a new column from the rownames
+  tibble::rownames_to_column("names") %>%
+  # Make a new names column with _LNA at the end of each sample name
+  mutate(names = paste(names, "_LNA", sep="")) 
+
+############### HNA
+###  MAKE NEW OTU TABLE FOR HNA SAMPLES
+otu_HNA_5in10 <- otu_table(HNA_physeq_5in10) %>%
+  data.frame() %>%
+  # Make a new column from the rownames
+  tibble::rownames_to_column("names") %>%
+  # Make a new names column with _LNA at the end of each sample name
+  mutate(names = paste(names, "_HNA", sep="")) 
+
+
+# Full join with original OTU table 
+otu_all_5in10 <- otu_table(rel_physeq_5in10) %>%
+  data.frame() %>%
+  # Make a new column from the rownames
+  tibble::rownames_to_column("names")
+
+## Combine the regular, HNA, and LNA otus all into the same otu table
+otu_table_ALL_5in10 <- full_join(otu_all_5in10, otu_LNA_5in10) %>%
+  full_join(otu_HNA_5in10) %>%
+  # Replace all of the NAs with zeros
+  mutate_all(funs(ifelse(is.na(.), 0, .))) %>%
+  # Make the names to be rownames
+  tibble::column_to_rownames("names")
+
+# compatible with phyloseq
+#### MUST BE A NUMBER AND NOT AN NA!
+# Combine all the data together into one object
+ALL_rel_physeq_5in10 <- merge_phyloseq(otu_table(otu_table_ALL_5in10, taxa_are_rows = FALSE), 
+                                   tax_table(as.matrix(tax_5seq_in_10perc)),
+                                   phy_tree(tree_5seq_in_10perc))
+
+
+
+################################################  
+################################################  WRITE OUT THE PHYLOSEQ OBJECT 
+save(list=c("ALL_rel_physeq_5in10","ALL_rel_physeq_1in3"), file=paste0("ALL-physeq-for-phylo.RData"))
+# This above line of code will create a single .RData object and with the 2 following phyloseq objects: 
+    # 1. ALL_rel_physeq_5in10
+    # 2. ALL_rel_physeq_1in3
+
+
 
 
 ########################################################################################  
