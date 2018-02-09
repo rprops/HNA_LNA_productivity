849129f8227efeb24e8e92e264a6a32ddc10a9c8
Marian L. Schmidt <marschmi@umich.edu>
2018-02-08 19:22:21 -0500

    Forgot to commit file to make all the data files

--- data/Chloroplasts_removed/ByLake_Filtering/taxonomic-levels.R
+++ data/Chloroplasts_removed/ByLake_Filtering/taxonomic-levels.R
@@ -145,32 +145,122 @@ ggsave("nums_tax_rankings.jpg", width = 7, height = 5)
 
 ############### CREATE NEW PHYLOSEQ OBJECTS
 rare_muskegon_physeq_5in10_rel
-# Species level
+# Relative Abundance
 species_musk_physeq_5in10 <- tax_glom(rare_muskegon_physeq_5in10_rel, taxrank = "Rank7")
 species_musk_physeq_5in10
+# Write files 
+write.table(otu_table(species_musk_physeq_5in10), file="5in10/muskegon/species/rel_muskegon_species_otu_5in10.tsv", row.names=TRUE)
+write.table(sample_data(species_musk_physeq_5in10)[,-1], file="5in10/muskegon/species/rel_muskegon_species_sampledata_5in10.tsv", row.names=TRUE)
+write.table(tax_table(species_musk_physeq_5in10), file="5in10/muskegon/species/rel_muskegon_species_taxonomy_5in10.tsv", row.names=TRUE)
+
+# Absolute abundance
+abs_species_musk_physeq_5in10 <- tax_glom(rare_muskegon_physeq_5in10_abs, taxrank = "Rank7")
+abs_species_musk_physeq_5in10
+# Write files
+write.table(otu_table(abs_species_musk_physeq_5in10), file="5in10/muskegon/species/abs_muskegon_species_otu_5in10.tsv", row.names=TRUE)
+write.table(sample_data(abs_species_musk_physeq_5in10)[,-1], file="5in10/muskegon/species/abs_muskegon_species_sampledata_5in10.tsv", row.names=TRUE)
+write.table(tax_table(abs_species_musk_physeq_5in10), file="5in10/muskegon/species/abs_muskegon_species_taxonomy_5in10.tsv", row.names=TRUE)
 
 # Family level
+# Relative Abundance
 family_musk_physeq_5in10 <- tax_glom(rare_muskegon_physeq_5in10_rel, taxrank = "Rank5")
 family_musk_physeq_5in10
+# Write files 
+write.table(otu_table(family_musk_physeq_5in10), file="5in10/muskegon/family/rel_muskegon_family_otu_5in10.tsv", row.names=TRUE)
+write.table(sample_data(family_musk_physeq_5in10)[,-1], file="5in10/muskegon/family/rel_muskegon_family_sampledata_5in10.tsv", row.names=TRUE)
+write.table(tax_table(family_musk_physeq_5in10), file="5in10/muskegon/family/rel_muskegon_family_taxonomy_5in10.tsv", row.names=TRUE)
+
+# Absolute abundance
+abs_family_musk_physeq_5in10 <- tax_glom(rare_muskegon_physeq_5in10_abs, taxrank = "Rank5")
+abs_family_musk_physeq_5in10
+# Write files
+write.table(otu_table(abs_family_musk_physeq_5in10), file="5in10/muskegon/family/abs_muskegon_family_otu_5in10.tsv", row.names=TRUE)
+write.table(sample_data(abs_family_musk_physeq_5in10)[,-1], file="5in10/muskegon/family/abs_muskegon_family_sampledata_5in10.tsv", row.names=TRUE)
+write.table(tax_table(abs_family_musk_physeq_5in10), file="5in10/muskegon/family/abs_muskegon_family_taxonomy_5in10.tsv", row.names=TRUE)
+
 
 #Phylum level
+# Relative Abundance
 phylum_musk_physeq_5in10 <- tax_glom(rare_muskegon_physeq_5in10_rel, taxrank = "Rank2")
 phylum_musk_physeq_5in10
+# Write files 
+write.table(otu_table(phylum_musk_physeq_5in10), file="5in10/muskegon/phylum/rel_muskegon_phylum_otu_5in10.tsv", row.names=TRUE)
+write.table(sample_data(phylum_musk_physeq_5in10)[,-1], file="5in10/muskegon/phylum/rel_muskegon_phylum_sampledata_5in10.tsv", row.names=TRUE)
+write.table(tax_table(phylum_musk_physeq_5in10), file="5in10/muskegon/phylum/rel_muskegon_phylum_taxonomy_5in10.tsv", row.names=TRUE)
+
+# Absolute abundance
+abs_phylum_musk_physeq_5in10 <- tax_glom(rare_muskegon_physeq_5in10_abs, taxrank = "Rank2")
+abs_phylum_musk_physeq_5in10
+# Write files
+write.table(otu_table(abs_phylum_musk_physeq_5in10), file="5in10/muskegon/phylum/abs_muskegon_phylum_5in10_otu_5in10.tsv", row.names=TRUE)
+write.table(sample_data(abs_phylum_musk_physeq_5in10)[,-1], file="5in10/muskegon/phylum/abs_muskegon_phylum_sampledata_5in10.tsv", row.names=TRUE)
+write.table(tax_table(abs_phylum_musk_physeq_5in10), file="5in10/muskegon/phylum/abs_muskegon_phylum_taxonomy_5in10.tsv", row.names=TRUE)
+
+### SAVE ALL THE PHYSEQS
+save(list=c("species_musk_physeq_5in10","family_musk_physeq_5in10", "phylum_musk_physeq_5in10"), file=paste0("5in10/muskegon/rel_tax_collapse_muskegon_5in10_physeqs.RData"))
+save(list=c("abs_species_musk_physeq_5in10","abs_family_musk_physeq_5in10", "abs_phylum_musk_physeq_5in10"), file=paste0("5in10/muskegon/abs_tax_collapse_muskegon_5in10_physeqs.RData"))
 
 
 
-
+##########
+#########
+##########
+### 1in3: Write the tsv files
 # Species
-rare_muskegon_physeq_1in3_rel
+# Relative Abundance
 species_musk_physeq_1in3 <- tax_glom(rare_muskegon_physeq_1in3_rel, taxrank = "Rank7")
 species_musk_physeq_1in3
+# Write files 
+write.table(otu_table(species_musk_physeq_1in3), file="1in3/muskegon/species/rel_muskegon_species_otu_1in3.tsv", row.names=TRUE)
+write.table(sample_data(species_musk_physeq_1in3)[,-1], file="1in3/muskegon/species/rel_muskegon_species_sampledata_1in3.tsv", row.names=TRUE)
+write.table(tax_table(species_musk_physeq_1in3), file="1in3/muskegon/species/rel_muskegon_species_taxonomy_1in3.tsv", row.names=TRUE)
 
-# Family
-rare_muskegon_physeq_1in3_rel
+# Absolute abundance
+abs_species_musk_physeq_1in3 <- tax_glom(rare_muskegon_physeq_1in3_abs, taxrank = "Rank7")
+abs_species_musk_physeq_1in3
+# Write files
+write.table(otu_table(abs_species_musk_physeq_1in3), file="1in3/muskegon/species/abs_muskegon_species_otu_1in3.tsv", row.names=TRUE)
+write.table(sample_data(abs_species_musk_physeq_1in3)[,-1], file="1in3/muskegon/species/abs_muskegon_species_sampledata_1in3.tsv", row.names=TRUE)
+write.table(tax_table(abs_species_musk_physeq_1in3), file="1in3/muskegon/species/abs_muskegon_species_taxonomy_1in3.tsv", row.names=TRUE)
+
+
+
+# Family level
+# Relative Abundance
 family_musk_physeq_1in3 <- tax_glom(rare_muskegon_physeq_1in3_rel, taxrank = "Rank5")
 family_musk_physeq_1in3
+# Write files 
+write.table(otu_table(family_musk_physeq_1in3), file="1in3/muskegon/family/rel_muskegon_family_otu_1in3.tsv", row.names=TRUE)
+write.table(sample_data(family_musk_physeq_1in3)[,-1], file="1in3/muskegon/family/rel_muskegon_family_sampledata_1in3.tsv", row.names=TRUE)
+write.table(tax_table(family_musk_physeq_1in3), file="1in3/muskegon/family/rel_muskegon_family_taxonomy_1in3.tsv", row.names=TRUE)
 
-# Phylum
+# Absolute abundance
+abs_family_musk_physeq_1in3 <- tax_glom(rare_muskegon_physeq_1in3_abs, taxrank = "Rank5")
+abs_family_musk_physeq_1in3
+# Write files
+write.table(otu_table(abs_family_musk_physeq_1in3), file="1in3/muskegon/family/abs_muskegon_family_otu_1in3.tsv", row.names=TRUE)
+write.table(sample_data(abs_family_musk_physeq_1in3)[,-1], file="1in3/muskegon/family/abs_muskegon_family_sampledata_1in3.tsv", row.names=TRUE)
+write.table(tax_table(abs_family_musk_physeq_1in3), file="1in3/muskegon/family/abs_muskegon_family_taxonomy_1in3.tsv", row.names=TRUE)
+
+
+#Phylum level
+# Relative Abundance
 phylum_musk_physeq_1in3 <- tax_glom(rare_muskegon_physeq_1in3_rel, taxrank = "Rank2")
 phylum_musk_physeq_1in3
+# Write files 
+write.table(otu_table(phylum_musk_physeq_1in3), file="1in3/muskegon/phylum/rel_muskegon_phylum_otu_1in3.tsv", row.names=TRUE)
+write.table(sample_data(phylum_musk_physeq_1in3)[,-1], file="1in3/muskegon/phylum/rel_muskegon_phylum_sampledata_1in3.tsv", row.names=TRUE)
+write.table(tax_table(phylum_musk_physeq_1in3), file="1in3/muskegon/phylum/rel_muskegon_phylum_taxonomy_1in3.tsv", row.names=TRUE)
+
+# Absolute abundance
+abs_phylum_musk_physeq_1in3 <- tax_glom(rare_muskegon_physeq_1in3_abs, taxrank = "Rank2")
+abs_phylum_musk_physeq_1in3
+# Write files
+write.table(otu_table(abs_phylum_musk_physeq_1in3), file="1in3/muskegon/phylum/abs_muskegon_phylum_1in3_otu_1in3.tsv", row.names=TRUE)
+write.table(sample_data(abs_phylum_musk_physeq_1in3)[,-1], file="1in3/muskegon/phylum/abs_muskegon_phylum_sampledata_1in3.tsv", row.names=TRUE)
+write.table(tax_table(abs_phylum_musk_physeq_1in3), file="1in3/muskegon/phylum/abs_muskegon_phylum_taxonomy_1in3.tsv", row.names=TRUE)
+
+### SAVE ALL THE PHYSEQS
+save(list=c("species_musk_physeq_1in3","family_musk_physeq_1in3", "phylum_musk_physeq_1in3"), file=paste0("1in3/muskegon/rel_tax_collapse_muskegon_1in3_physeqs.RData"))
+save(list=c("abs_species_musk_physeq_1in3","abs_family_musk_physeq_1in3", "abs_phylum_musk_physeq_1in3"), file=paste0("1in3/muskegon/abs_tax_collapse_muskegon_1in3_physeqs.RData"))
 
